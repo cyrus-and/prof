46851e7dc27d8590aafe24f8337841c396a205cc
Andrea Cardaci <cyrus.and@gmail.com>
2015-06-26 16:07:32 +0200

    Minor doc fix

--- prof.h
+++ prof.h
@@ -6,10 +6,10 @@
  *
  * Prof offers a quick way to measure performance events (CPU clock cycles,
  * cache misses, branch mispredictions, etc.) of C/C++ code snippets. Prof is
- * just a quick wrapper around the `perf_event_open` system call, its main goal
- * is to be easy to setup and painless to use for targeted optimizations,
- * namely, when the hot spot has already been identified. In no way Prof is a
- * replacement for a fully-fledged profiler like perf, gprof, callgrind, etc.
+ * just a wrapper around the `perf_event_open` system call, its main goal is to
+ * be easy to setup and painless to use for targeted optimizations, namely, when
+ * the hot spot has already been identified. In no way Prof is a replacement for
+ * a fully-fledged profiler like perf, gprof, callgrind, etc.
  *
  * Please be aware that Prof uses `__attribute__((constructor))` to be as more
  * straightforward to setup as possible, so it cannot be included more than
